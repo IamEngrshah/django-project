6bf8e860ae001178cadc907c679e66fa6fa4520e
Faheem Shehzad <faheemshehzad@joyngroup.us>
2023-04-15 23:57:58 +0500

    ADDED: Home page

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from fshec2.main import load_path
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    # return HttpResponse('Hello')
+    return render(request, 'index.html')
 
 
 def hello(request):
+    load_path()
     return HttpResponse("Hello, world!")
